8092027ea75cf91b8da204671b1e28d06b66a5e8
Marcin Rogulski <m.rogulski@omtech.pl>
2018-10-16 14:07:08 +0200

    prime ng attempt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./common-components/login/login.component";
+
+import { AppRoutingModule } from "./modules/app-routing/app-routing.module";
+import { MessagesComponent } from "./common-components/messages/messages.component";
+
+import { AccordionModule } from "primeng/components/accordion/accordion";
+import { PanelModule } from "primeng/components/panel/panel";
+import { ButtonModule } from "primeng/components/button/button";
+import { RadioButtonModule } from "primeng/components/radioButton/radioButton";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, MessagesComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AccordionModule,
+    PanelModule,
+    ButtonModule,
+    RadioButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "../../common-components/login/login.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/", pathMatch: "full" },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
