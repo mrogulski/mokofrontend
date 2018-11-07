7bbdbe76eebc315c1f49f4a37efb6b108fde0e2b
mrogulski <m.rogulski@outlook.com>
2018-11-07 18:59:29 +0100

    navbar improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { AuthGuardService } from "./services/auth-guard-service/auth-guard.service";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,10 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "mokofrontend";
+  canActivate: boolean;
+
+  constructor(authguard: AuthGuardService){
+    this.canActivate = authguard.canActivate();
+
+  }
 }
